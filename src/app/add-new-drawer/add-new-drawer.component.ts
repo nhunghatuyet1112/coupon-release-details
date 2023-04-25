4a53340a86142de4eee3177bf0e1b97e06bfa34a
nhunghatuyet1112 <nhunghatuyet111202@gmail.com>
2023-04-25 12:38:34 +0700

    NewCouponProduct-JS-Add: Thêm script xử lý thay đổi thông tin sản phẩm

--- src/app/add-new-drawer/add-new-drawer.component.ts
+++ src/app/add-new-drawer/add-new-drawer.component.ts
@@ -7,27 +7,71 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class AddNewDrawerComponent {
   //VARIABLES RECEIVE VALUE FROM APP-MAIN-CONTENT
+
+  //TEXTBOX
   @Input() productName = '';
   @Input() Barcode = '';
+  //TEXTBOX
+
+  //IMAGE
   @Input() ImageUrl = "/Uploads/_11/product1/4909411076870.jpg";
+  //IMAGE
+
+  //PRODUCT LINK
+  @Input() Alias = '';
+  //PRODUCT LINK
+
+  //DROPDOWN STATUS ID LIST
+  @Input() statusID: Array<{ text: string, value: number }> = [];
+  // @Input() defaultStatus: number;
+  //DROPDOWN STATUS ID LIST
+
+  //PRICE TEXTBOXES VALUE
+  @Input() Price = 0;
+  @Input() PriceBase = 0;
+  @Input() PriceVIP = 0;
+  //PRICE TEXTBOXES VALUE
+
+
   //VARIABLES RECEIVE VALUE FROM APP-MAIN-CONTENT
 
   //FUNCTIONS PASS TO APP-MAIN-CONTENT
-  @Output() closeEditDrawer = new EventEmitter();
-  @Output() editProductClick = new EventEmitter();
-  @Output() changeProductName = new EventEmitter<string>();
+  @Output() closeDrawer = new EventEmitter();
+  @Output() submitProduct = new EventEmitter();
+
+  //FIND PRODUCT BY BARCODE
+  @Output() changeBarcode = new EventEmitter<string>();
+  @Output() blurBarcode = new EventEmitter();
+  //FIND PRODUCT BY BARCODE
+
+
+  //CHANGE PRODUCT PRICE
+  @Output() changePrice = new EventEmitter();
+  //CHANGE PRODUCT PRICE
+  @Output() changeStatusID = new EventEmitter();
 
   onCloseDrawer(event: any) {
-    this.closeEditDrawer.emit(event);
+    this.closeDrawer.emit(event);
   }
 
-  onEditProduct(event: any) {
-    this.editProductClick.emit(event);
+  onSubmitProduct(event: any) {
+    this.submitProduct.emit(event);
   }
 
-  onChangeName(value: string) {
-    this.changeProductName.emit(value);
+  onBlurBarcode(event: any) {
+    this.blurBarcode.emit(event);
+  }
+
+  onChangeBarcode(value: string) {
+    this.changeBarcode.emit(value);
+  }
+
+  onChangePrice(event: any) {
+    this.changePrice.emit(event);
+  }
+
+  onChangeStatusID(event: any) {
+    this.changeStatusID.emit(event);
   }
   //FUNCTIONS PASS TO APP-MAIN-CONTENT
-
 }
