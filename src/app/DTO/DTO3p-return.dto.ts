da2b255ff5870d2b687aa62ed4d2ec2a76a09048
nhunghatuyet1112 <nhunghatuyet111202@gmail.com>
2023-04-25 12:32:44 +0700

    NewCouponProduct-DTO-Add: Thêm DTO của API Update Product

--- src/app/DTO/DTO3p-return.dto.ts
+++ src/app/DTO/DTO3p-return.dto.ts
@@ -1,5 +1,21 @@
-export class Product {
+export class PRODUCT {
     StatusCode: number;
-    ErrorString: string;
+    ErrorString: null;
     ObjectReturn: any;
+
+    constructor(statusCode: number, errorString: null, objectReturn) {
+        this.StatusCode = statusCode;
+        this.ErrorString = errorString;
+        this.ObjectReturn = objectReturn;
+    }
+}
+
+export class newProduct {
+    DTO: any;
+    Properties: string[];
+
+    constructor(dto, properties: Array<string>) {
+        this.DTO = dto;
+        this.Properties = properties;
+    }
 }
