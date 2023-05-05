f734b83c8d9cb301e52c3752da972066af0e1d05
nhunghatuyet1112 <nhunghatuyet111202@gmail.com>
2023-05-05 14:14:59 +0700

    Workstation-DTO-Add: Thêm dto điểm làm việc

---
+++ src/app/DTO/DTO3p-workstation.dto.ts
@@ -0,0 +1,90 @@
+export class Workstation {
+    name: string;
+    code: string;
+    shortName?: string;
+    dependentPlace?: string;
+    address?: string;
+    city?: string;
+    district?: string;
+    ward?: string;
+    descr?: string;
+    status?: string;
+    content?: Workstation[];
+    constructor(name, code, shortName, dependentPlace, address, city, district, ward, descr, status) {
+        this.name = name;
+        this.code = code;
+        this.shortName = shortName;
+        this.dependentPlace = dependentPlace;
+        this.address = address;
+        this.city = city;
+        this.district = district;
+        this.ward = ward;
+        this.descr = descr;
+        this.status = status;
+    }
+}
+
+export const WORKSTATION: Workstation[] = [
+    {
+        name: 'Văn phòng',
+        code: 'VP',
+        content: [
+            {
+                name: 'Văn phòng 40',
+                code: 'HW40',
+                shortName: 'VP Head',
+                dependentPlace: 'Văn phòng',
+                address: '40 đường số 14',
+                city: 'Tp.Hồ Chí Minh',
+                district: 'Quận Gò Vấp',
+                ward: 'Phường 5',
+                descr: '',
+                status: 'Đang áp dụng'
+            },
+            {
+                name: 'Văn phòng 42',
+                code: 'LO04',
+                shortName: 'VP Head',
+                dependentPlace: 'Văn phòng',
+                address: '40 đường số 14',
+                city: 'Tp.Hồ Chí Minh',
+                district: 'Quận Gò Vấp',
+                ward: 'Phường 5',
+                descr: '',
+                status: 'Đang áp dụng'
+            },
+        ]
+    },
+    {
+
+        name: 'Kho',
+        code: 'DP',
+        content: [
+            {
+                name: 'Kho 42',
+                code: 'DP01',
+                shortName: 'VP Head',
+                dependentPlace: 'Kho',
+                address: '40 đường số 14',
+                city: 'Tp.Hồ Chí Minh',
+                district: 'Quận Gò Vấp',
+                ward: 'Phường 5',
+                descr: '',
+                status: 'Đang áp dụng'
+            },
+            {
+                name: 'Kho online',
+                code: 'ON01',
+                shortName: 'VP Head',
+                dependentPlace: 'Kho',
+                address: '40 đường số 14',
+                city: 'Tp.Hồ Chí Minh',
+                district: 'Quận Gò Vấp',
+                ward: 'Phường 5',
+                descr: '',
+                status: 'Đang áp dụng'
+            },
+        ]
+    },
+
+]
